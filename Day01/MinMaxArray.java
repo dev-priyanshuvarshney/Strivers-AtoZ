2f0c9d933115b68c629dbcd978bc08224a16860a
Priyansh27u <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-10-13 22:20:11 +0530

    firsCode

---
+++ Day01/MinMaxArray.java
@@ -0,0 +1,7 @@
+package Day01;
+
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int
+    }
+}
---
+++ Day01/Reverse.java
@@ -0,0 +1,21 @@
+package Day01;
+public class Reverse{
+    public static void reverseArray(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while (left<right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        reverseArray(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
