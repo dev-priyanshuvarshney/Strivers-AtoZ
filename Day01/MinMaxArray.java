8d4f9cf0c55a2d4485b0b9ac10c25e92ea104a2d
Priyansh27u <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-10-13 22:29:47 +0530

    second

--- Day01/MinMaxArray.java
+++ Day01/MinMaxArray.java
@@ -1,7 +1,23 @@
 package Day01;
 
 public class MinMaxArray {
+    public static int[] minMax(int[] arr){
+        int n = arr.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            } else if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        return new int[]{min, max};
+    }
     public static void main(String[] args) {
-        int
+        int[] arr = {4,8,9,7,6,3};
+        int[] result = minMax(arr);
+        System.out.println("Min: "+result[0]);
+        System.out.println("Max: "+result[1]);
     }
 }
