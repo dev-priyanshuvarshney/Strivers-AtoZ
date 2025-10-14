c9547a4fe0d7ce0610f84fafd8cb2581fdc28ada
Priyansh27u <168388694+dev-priyanshuvarshney@users.noreply.github.com>
2025-10-15 00:04:48 +0530

    day2

--- Day02/KthLargest.java
+++ Day02/KthLargest.java
@@ -1,21 +1,14 @@
 package Day02;
 
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 public class KthLargest {
-    public static int klargest(int[] arr, int k){
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k+1);
-        for(int i : arr){
-            pq.add(i);
-            if (pq.size()<k) {
-                pq.poll();
-            }
-        }
-        return pq.poll();
-    }
     public static void main(String[] args) {
-        int[] arr = {4,2,5,8,7,6};
-        int k=2;
-        klargest(arr, k);
+        int[] arr = {4,57,8,6,2,5};
+        int k = 2;
+        Arrays.sort(arr);
+        int n = arr[arr.length-k];
+        System.out.println(n);
+
     }
 }
